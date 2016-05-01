85ebf184f7a239b9ef251c28e1ffbd6d50b3ceb3
AxMari <AlexisMarien@gmail.com>
2016-05-01 17:51:54 -0400

    sunday

--- app.js
+++ app.js
@@ -1,1 +1,84 @@
-console.log("Hello, World!");
+
+$(document).ready(function(){
+
+
+$("#seatform").hide();
+
+
+$(".available").each(function() {
+	$(this).on('click', function(){
+		$('#seatform').show();
+
+});	
+});
+
+$('#button').click(function(){
+var name = $("#name").val(); 
+var email = $('#email').val();		
+var seat = $('#seat').val();
+var p = $("#seats > p").text();
+
+		console.log(name);
+		console.log(email);
+		console.log(seat);
+		console.log(p);
+		// console.log(number);
+
+		
+	
+		$('.available').addClass("reserved");
+	$('.reserved').removeClass('available');	
+});
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+// $('.seats').append('<div class="chair">');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
